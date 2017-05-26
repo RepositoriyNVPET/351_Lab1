cc9825302a1bf7309fea6c05ed6adb45eb12e1bc
AnastasiaZhihadlo <nastia444440@gmail.com>
2017-05-26 12:00:42 +0300

    Update Untitled1.cpp

--- 1.4/Untitled1.cpp
+++ 1.4/Untitled1.cpp
@@ -10,7 +10,7 @@ int main()
 	setlocale (LC_CTYPE, "ukr");
 	cout<<"Вывод а:";
 	cin>>a;
-	cout<<"Вывод b:";
+	cout<<" b:";
 	cin>>b;
 	a=sqrt(pow(a,2)+pow(b,2));
 	printf("Вывод на экран: %.2f",a);
